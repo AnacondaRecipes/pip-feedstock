85a72fe73779e4c33aa0ca1c3cdee695041db0fb
Jonathan Helmus <jjhelmus@gmail.com>
2024-07-19 14:58:38 -0500

    add option to bootstrap from pip whl

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,5 +1,10 @@
-set PYTHONPATH=".\src"
-%PYTHON% -m pip install . --no-deps --no-build-isolation -vv
+if "%BOOTSTRAP_FROM_WHL%"=="yes" (
+    set PYTHONPATH=".\pip-%PKG_VERSION%-py3-none-any.whl"
+    %PYTHON% -m pip install --no-cache-dir --no-index --no-index --find-links . pip
+) else (
+    set PYTHONPATH=".\src"
+    %PYTHON% -m pip install . --no-deps --no-build-isolation -vv
+)
 if errorlevel 1 exit 1
 
 cd %SCRIPTS%
--- recipe/build.sh
+++ recipe/build.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
-# use the pip source to install itself
-PYTHONPATH="./src" $PYTHON -m pip install . --no-deps --no-build-isolation -vv
+if [[ ${BOOTSTRAP_FROM_WHL} == yes ]]; then
+    # use the pip wheel to install itself
+    export PYTHONPATH='./pip-${PKG_VERSION}-py3-none-any.whl'
+    $PYTHON -m pip install --no-cache-dir --no-index --no-index --find-links . pip
+else
+    # use the pip source to install itself
+    export PYTHONPATH="./src"
+    $PYTHON -m pip install . --no-deps --no-build-isolation -vv
+fi
+
 
 cd $PREFIX/bin
 rm -f pip2* pip3*
